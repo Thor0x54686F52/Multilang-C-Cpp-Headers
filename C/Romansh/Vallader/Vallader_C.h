fa14918e3716720be633d23f34fda3f0b7a3ed61
Thor0x54686F52 <oberhummer2.0@gmail.com>
2025-05-12 19:22:02 +0200

    Improvement in Vallader and German

--- C/Romansh/Vallader/Vallader_C.h
+++ C/Romansh/Vallader/Vallader_C.h
@@ -15,65 +15,74 @@
   it’s just the ASCII mapping needed for C/C++ macros.
 */
 
-#define automatic             auto
-#define interromper           break
-#define cas                   case
-#define caracter              char
-#define constanta             const
-#define cuntinuar             continue
-#define predeterminad         default
-#define fai                   do
-#define flotant_precis        double
-#define altriment             else
-#define enumeraziun           enum
-#define dafor                 extern
-#define flotant               float
-#define per                   for
-#define ir_a                  goto
-#define sche                  if 
-#define numer_entir           int
-#define lung                  long
-#define retornar              return
-#define numer_entir_curt      short
-#define signa                 signed       // in Vallader you should use 'signà', but you can't use 'à' in the definition name
-#define dimensiun_da          sizeof
-#define static                static
-#define structura             struct
-#define tscherna              switch
-#define definiziun_tip        typedef
-#define reuenion              union       // in Vallader you should use 'reünion', but you can't use 'ü' in the definition name
-#define senza_sign            unsigned
-#define nagin                 void
-#define instabel              volatile
-#define mentr                 while
-#define registrar             register
+#define automatic				auto
+#define interromper				break
+#define cas						case
+#define caracter				char
+#define constanta				const
+#define cuntinuar				continue
+#define predeterminad			default
+#define fai						do
+#define flotant_precis			double
+#define enumeraziun				enum
+#define dafor					extern
+#define flotant					float
+#define per						for
+#define ir_a					goto
+#define sche					if
+#define altriment				else
+#define numer_entir				int
+#define lung					long
+#define retornar				return
+#define numer_entir_curt		short
+#define signa					signed       // in Vallader you should use 'signà', but you can't use 'à' in the definition name
+#define dimensiun_da			sizeof
+#define static					static
+#define structura				struct
+#define tscherna				switch
+#define definiziun_tip			typedef
+#define reuenion				union       // in Vallader you should use 'reünion', but you can't use 'ü' in the definition name
+#define senza_sign				unsigned
+#define nagin					void
+#define instabel				volatile
+#define mentr					while
+#define registrar				register
 
-#define Allinia_sco           _Alignas
-#define dimensiun_align       _Alignof
-#define atomic                _Atomic
-#define bitarint              _BitInt
-#define bolean                _Bool
-#define complex               _Complex
-#define decimal32             _Decimal32
-#define decimal64             _Decimal64
-#define decimal128            _Decimal128
-#define generic               _Generic
-#define imaginari             _Imaginary
-#define sen_retornar          _Noreturn
-#define assert_static         _Static_assert
-#define fil_locala            _Thread_local
+#define Allinia_sco				_Alignas
+#define dimensiun_align			_Alignof
+#define atomic					_Atomic
+#define bitarint				_BitInt
+#define bolean					_Bool
+#define complex					_Complex
+#define decimal32				_Decimal32
+#define decimal64				_Decimal64
+#define decimal128				_Decimal128
+#define generic					_Generic
+#define imaginari				_Imaginary
+#define sen_retornar			_Noreturn
+#define assert_static			_Static_assert
+#define fil_locala				_Thread_local
 
 #define funcziun_principala   main
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <time.h>
 
-#define stampetta             printf
-#define leg_im                scanf
-#define avra_file             fopen
-#define serr_file             fclose
+#define tempra					time
 
-#define sortida_success       EXIT_SUCCESS
-#define sortida_error         EXIT_FAILURE
+#define Legir_in_caracter		getchar
+
+#define stampetta				printf
+#define leg_im					scanf
+#define avra_file				fopen
+#define serr_file				fclose
+
+#define nettaScherm()			printf("\33c")
+#define imposta_UTF_8()			setlocale(LC_ALL, "de_DE.utf-8")
+
+#define SORTIDA_SUCCESS			EXIT_SUCCESS
+#define SORTIDA_ERROR			EXIT_FAILURE
 
 #endif
